920d15653949d6d5f86de2ac2d662c0ce7de0a38
Maria2721 <84140203+Maria2721@users.noreply.github.com>
2023-12-13 22:46:31 +0300

    Initial commit

---
+++ src/store/countries/countries-selectors.js
@@ -0,0 +1,16 @@
+export const selectCountriesInfo = (state) => ({
+  status: state.countries.status,
+  error: state.countries.error,
+  qty: state.countries.list.length,
+});
+
+export const selectAllCountries = (state) => state.countries.list;
+
+// добавить троттлинг
+export const selectVisibleCountries = (state, { search = "", region = "" }) => {
+  return state.countries.list.filter(
+    (country) =>
+      country.name.toLowerCase().includes(search.toLowerCase()) &&
+      country.region.includes(region)
+  );
+};
---
+++ src/store/details/details-selector.js
@@ -0,0 +1,3 @@
+export const selectCurrentCountry = (state) => state.details.currentCountry;
+export const selectDetilas = (state) => state.details;
+export const selectNeighbors = (state) => state.details.neighbors;
